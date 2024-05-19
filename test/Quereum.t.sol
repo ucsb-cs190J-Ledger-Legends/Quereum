d1d5bae57d04642eaa307d3a82641aed2b14b063
Ved Pradhan <vedprad1@gmail.com>
2024-05-19 12:55:59 -0700

    Add Quereum files.

---
+++ test/Quereum.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Counter} from "../src/Counter.sol";
+
+contract QuereumTest is Test {
+    Quereum public quereum;
+
+    
+}
