8ea95f2ca7dc8412f976734072a4076928163b44
Ved Pradhan <vedprad1@gmail.com>
2024-05-19 13:26:56 -0700

    Implement function to claim reward.

--- src/Quereum.sol
+++ src/Quereum.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+contract Quereum {
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+    mapping(address => uint256) private balances; // The balance of each user.
 
-    function increment() public {
-        number++;
+    // This function allows a user to claim any reward
+    // balance the app owns them.
+    function claim_reward() public {
+        require(balances[msg.sender] > 0, "No reward to claim.");
+
+        uint256 amountToSend = balances[msg.sender];
+        balances[msg.sender]; // Reset to prevent re-entrancy.
+
+        (bool confirmation, ) = msg.sender.call{value: amountToSend}("");
+        require(confirmation, "Ether was not received properly.")
     }
 }
